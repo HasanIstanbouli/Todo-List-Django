528e42b46431dec32935f69e74ff4586d4ad7885
Hasan Istanbouli <istanbouli.hasan@gmail.com>
2025-07-22 18:26:09 +0200

    #4: pre-commit fixes

--- Todo_List_App/templatetags/custom_filters.py
+++ Todo_List_App/templatetags/custom_filters.py
@@ -2,6 +2,7 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
 def toggle_order(current_order):
-    return 'asc' if current_order == 'desc' else 'desc'
+    return "asc" if current_order == "desc" else "desc"
