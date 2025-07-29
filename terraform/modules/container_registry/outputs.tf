5d8fcda5d13adbd3e8e58367f8c70f17b5a8fe03
Hasan Istanbouli <istanbouli.hasan@gmail.com>
2025-07-29 16:47:57 +0200

    #22: add container registry terraform module

---
+++ terraform/modules/container_registry/outputs.tf
@@ -0,0 +1,9 @@
+output "endpoint" {
+  value = digitalocean_container_registry.todo_container_registry.endpoint
+}
+output "id" {
+  value = digitalocean_container_registry.todo_container_registry.id
+}
+output "name" {
+  value = digitalocean_container_registry.todo_container_registry.name
+}
