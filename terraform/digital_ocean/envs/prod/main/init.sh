c8bb602dfc6380ab33d756e78d6ef6dbb9c47bad
Hasan Istanbouli <istanbouli.hasan@gmail.com>
2025-09-07 17:51:22 +0200

    fix env-var name of terraform init.sh file in production

--- terraform/digital_ocean/envs/prod/main/init.sh
+++ terraform/digital_ocean/envs/prod/main/init.sh
@@ -2,7 +2,7 @@
 
 # Load from environment (could come from .env file or CI/CD secrets)
 : "${TF_PROD_BACKEND_BUCKET:?Missing TF_PROD_BACKEND_BUCKET}"
-: "${TF_PROD_APP_INFRA_BACKEND_KEY:?Missing TF_PROD_APP_INFRA_BACKEND_KEY}"
+: "${TF_PROD_MAIN_APP_BACKEND_KEY:?Missing TF_PROD_MAIN_APP_BACKEND_KEY}"
 : "${TF_PROD_BACKEND_REGION:?Missing TF_PROD_BACKEND_REGION}"
 # For S3 to work...
 : "${AWS_ACCESS_KEY_ID:?Missing AWS_ACCESS_KEY_ID}"
@@ -13,5 +13,5 @@
 
 terraform init \
   -backend-config="bucket=${TF_PROD_BACKEND_BUCKET}" \
-  -backend-config="key=${TF_PROD_APP_INFRA_BACKEND_KEY}" \
+  -backend-config="key=${TF_PROD_MAIN_APP_BACKEND_KEY}" \
   -backend-config="region=${TF_PROD_BACKEND_REGION}"
