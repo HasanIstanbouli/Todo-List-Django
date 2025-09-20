edcc98f24dc347f8920b2f51da4a6d70f03ec546
Hasan Istanbouli <istanbouli.hasan@gmail.com>
2025-09-21 01:41:17 +0200

    #41: add new outputs to RDS module

--- terraform/aws/modules/rds_postgres/outputs.tf
+++ terraform/aws/modules/rds_postgres/outputs.tf
@@ -7,3 +7,13 @@ output "db_port" {
 output "db_identifier" {
   value = aws_db_instance.this.id
 }
+output "db_name" {
+  value = aws_db_instance.this.db_name
+}
+output "db_username" {
+  value = aws_db_instance.this.username
+}
+output "db_password" {
+  value     = aws_db_instance.this.password
+  sensitive = true
+}
